0858656ae41653d24e09640c72f7916e6774ebbd
Fabio Torres <fabiotorres.adv@gmail.com>
2022-02-09 09:25:14 -0500

    vategories vs products

--- site.php
+++ site.php
@@ -1,6 +1,8 @@
 <?php
 
 use \Hcode\Page;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
 use \Hcode\Model\Product;
 
 $app->get('/', function () {
@@ -12,6 +14,22 @@ $app->get('/', function () {
         "products"=>Product::checkList($products)
     ]);
 
+});
+
+$app->get("/categories/:idcategory", function ($idcategory) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new Page();
+
+    $page->setTpl("category", [
+        "category"=>$category->getValues(),
+        "product"=>Product::checkList($category->getProducts())
+    ]);
 });
 
 ?>
